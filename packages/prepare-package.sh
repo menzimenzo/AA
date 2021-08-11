372b2d6eb9be63cfaf24ee50ee688a154eeb0ea3
lscherer <laurent_scherer@hotmail.com>
2021-08-11 12:09:22 +0200

    Packaging V1.1.3

--- packages/prepare-package.sh
+++ packages/prepare-package.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Shell de préparation du package de déploiement pour livraison
-echo "ATTENTION, il faut positionner le CLIENT_ID à vide dans le fichier config.js du backend avant de faire le package"
 echo "Pensez à changer le numéro de version dans les fichiers package.json du frontend ET backend et la version dans le fichier footer.vue fu front-end/components"
 
 cp -r ../front-end ./
@@ -16,7 +15,7 @@ cp -r ../mail-server ./
 #rm -r ./mail-server/node_modules
 rm ./mail-server/pm2.integ.json
 
-tar -zcvf aaq-1.1.2.tar.gz  front-end back-end mail-server
+tar -zcvf aaq-1.1.3.tar.gz  front-end back-end mail-server
 
 rm -r ./front-end
 rm -r ./back-end
